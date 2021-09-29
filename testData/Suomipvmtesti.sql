8bccb64d1438598ce4199b68d15c5fec5447073d
Mika Vainio <koti.mika.vainio@gmail.com>
2021-09-29 11:32:59 +0300

    Lisätty kannanluotiskripti
    Kanta + taulut + näkymät + data

--- testData/Suomipvmtesti.sql
+++ testData/Suomipvmtesti.sql
@@ -1,3 +1,2 @@
 INSERT INTO vuokraus (yksilointikoodi,asiakasnumero,alkamispaiva,paattymispaiva) VALUES
-	 (1,1,'2021-09-15','2021-09-22'),
-	 (2,2,'2021-09-16','2021-09-17');
+(3,1,'29.9.2021','5.10.2021')
